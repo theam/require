abb84c330bfb7ca4e15f3fafdc958db6f4e75b3e
Nikita Tchayka <nikitatchayka@gmail.com>
2018-05-29 16:27:51 +0100

    Bump

--- benchmark/Main.hs
+++ benchmark/Main.hs
@@ -1,6 +1,7 @@
 -- You can benchmark your code quickly and effectively with Criterion. See its
 -- website for help: <http://www.serpentine.com/criterion/>.
-import Criterion.Main
+
+import Universum
 
 main :: IO ()
-main = defaultMain [bench "const" (whnf const ())]
+main = putTextLn "Hello"
