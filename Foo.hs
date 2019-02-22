5c90497fd0d4af4875a796496ea27789e6aa83bf
Nikita Tchayka <nikitatchayka@gmail.com>
2019-02-22 15:37:05 +0000

    Remove Foo files

--- Foo.hs
+++
@@ -1,3 +0,0 @@
-module Foo where
-require Data.IORef --a comment
-import Data.Text
--- Foo1.hs
+++
@@ -1,6 +0,0 @@
-{-# LINE 1 "Foo.hs" #-}
-module Foo where
-import Data.IORef (IORef)
-{-# LINE 2 "Foo.hs" #-}
-import qualified Data.IORef as IORef
-import Data.Text
