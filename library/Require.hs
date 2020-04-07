170c4fede1e554af3b743ba4fc07fc01b89a2667
Nikita Tchayka <nick@booster.cloud>
2020-04-07 13:50:36 +0100

    Fix arg flipping

--- library/Require.hs
+++ library/Require.hs
@@ -1,26 +1,29 @@
 module Require where
 
-import Universum
-
-import qualified Text.Megaparsec      as Megaparsec
-import qualified Text.Megaparsec.Char as Megaparsec
-import qualified Data.Text            as Text
+import qualified Data.Text as Text
 import Options.Generic
+import Relude
 import System.Directory
+import qualified Text.Megaparsec as Megaparsec
+import qualified Text.Megaparsec.Char as Megaparsec
 
-newtype FileName   = FileName { unFileName :: Text }
+newtype FileName = FileName {unFileName :: Text}
+
 newtype LineNumber = LineNumber Int
-type Parser        = Megaparsec.Parsec Void Text
 
-data RequireInfo = RequireInfo
-  { riFullModuleName :: Text
-  , riModuleAlias    :: Text
-  , riImportedTypes  :: Maybe [Text]
-  } deriving Show
+type Parser = Megaparsec.Parsec Void Text
 
-data CommandArguments =
-  CommandArguments Text Text Text
-  deriving Generic
+data RequireInfo
+  = RequireInfo
+      { riFullModuleName :: Text,
+        riModuleAlias :: Text,
+        riImportedTypes :: Maybe [Text]
+      }
+  deriving (Show)
+
+data CommandArguments
+  = CommandArguments Text Text Text
+  deriving (Generic)
 
 instance ParseRecord CommandArguments
 
@@ -29,24 +32,25 @@ findRequires = do
   currentDir <- getCurrentDirectory
   files <- getDirectoryContents currentDir
   let textFiles = fmap toText files
-  return $ head <$> (nonEmpty $ filter (Text.isSuffixOf "Requires") textFiles)
+  return $ head <$> nonEmpty (filter (Text.isSuffixOf "Requires") textFiles)
 
 requireMain :: IO ()
 requireMain = do
-  CommandArguments _ inputFile outputFile <- getRecord "Require Haskell preprocessor" :: IO CommandArguments
+  CommandArguments inputFile _ outputFile <- getRecord "Require Haskell preprocessor" :: IO CommandArguments
   content <- readFile (toString inputFile)
   writeFile
     (toString outputFile)
-    ( Require.transform
-        False
-        (Require.FileName inputFile)
-        ""
-        content
+    ( toString $
+        Require.transform
+          False
+          (Require.FileName inputFile)
+          ""
+          (toText content)
     )
 
 autorequireMain :: IO ()
 autorequireMain = do
-  CommandArguments _ inputFile outputFile <- getRecord "Require Haskell preprocessor" :: IO CommandArguments
+  CommandArguments inputFile _ outputFile <- getRecord "Require Haskell preprocessor" :: IO CommandArguments
   requiresFile <- findRequires
   case requiresFile of
     Nothing -> die "There is no Requires file in the system"
@@ -55,97 +59,97 @@ autorequireMain = do
       content <- readFile (toString inputFile)
       writeFile
         (toString outputFile)
-        (Require.transform
-          True
-          (Require.FileName inputFile)
-          file
-          content)
+        ( toString $
+            Require.transform
+              True
+              (Require.FileName inputFile)
+              (toText file)
+              (toText content)
+        )
 
 transform :: Bool -> FileName -> Text -> Text -> Text
 transform autorequireEnabled filename imports input
   | autorequireEnabled = transform' True filename imports input
-  | noAutorequire  = transform' False filename imports input
-  | otherwise      = transform' True filename imports input
- where
-  noAutorequire = (length $ filter (\t -> "autorequire" `Text.isPrefixOf` t) $ lines input) == 0
-
+  | noAutorequire = transform' False filename imports input
+  | otherwise = transform' True filename imports input
+  where
+    noAutorequire = not (any ("autorequire" `Text.isPrefixOf`) $ lines input)
 
 transform' :: Bool -> FileName -> Text -> Text -> Text
 transform' shouldPrepend filename prepended input =
   Text.lines input
-  &   filter (\t -> not $ "autorequire" `Text.isPrefixOf` t)
-  &   zip [1..]
-  >>= prependAfterModuleLine
-  <&> (\(ln, text) -> maybe (text <> "\n") (renderImport filename (LineNumber ln)) $ Megaparsec.parseMaybe requireParser text)
-  &   (lineTag filename (LineNumber 1) :)
-  &   Text.concat
- where
-  enumeratedPrepend ln
-   | shouldPrepend = zip (repeat ln) (Text.lines prepended)
-   | otherwise     = []
-  prependAfterModuleLine (ln, text)
-   | ("module" `Text.isPrefixOf` text)
-     && ("where" `Text.isSuffixOf`) text = (ln, text) : enumeratedPrepend ln
-   | ("instance" `Text.isPrefixOf` text)
-     && ("where" `Text.isSuffixOf`) text = [(ln, text)]
-   | ("data" `Text.isPrefixOf` text)
-     && ("where" `Text.isSuffixOf`) text = [(ln, text)]
-   | ("class" `Text.isPrefixOf` text)
-     && ("where" `Text.isSuffixOf`) text = [(ln, text)]
-   | (not $ "instance" `Text.isPrefixOf` text)
-     && (not $ "class" `Text.isPrefixOf` text)
-     && (not $ "data" `Text.isPrefixOf` text)
-     && ("where" `Text.isPrefixOf`) text = (ln, text) : enumeratedPrepend (ln)
-   | otherwise                      = [(ln, text)]
+    & filter (\t -> not $ "autorequire" `Text.isPrefixOf` t)
+    & zip [1 ..]
+    >>= prependAfterModuleLine
+    <&> (\(ln, text) -> maybe (text <> "\n") (renderImport filename (LineNumber ln)) $ Megaparsec.parseMaybe requireParser text)
+    & (lineTag filename (LineNumber 1) :)
+    & Text.concat
+  where
+    enumeratedPrepend ln
+      | shouldPrepend = zip (repeat ln) (Text.lines prepended)
+      | otherwise = []
+    prependAfterModuleLine (ln, text)
+      | ("module" `Text.isPrefixOf` text)
+          && ("where" `Text.isSuffixOf`) text =
+        (ln, text) : enumeratedPrepend ln
+      | ("instance" `Text.isPrefixOf` text)
+          && ("where" `Text.isSuffixOf`) text =
+        [(ln, text)]
+      | ("data" `Text.isPrefixOf` text)
+          && ("where" `Text.isSuffixOf`) text =
+        [(ln, text)]
+      | ("class" `Text.isPrefixOf` text)
+          && ("where" `Text.isSuffixOf`) text =
+        [(ln, text)]
+      | not ("instance" `Text.isPrefixOf` text)
+          && not ("class" `Text.isPrefixOf` text)
+          && not ("data" `Text.isPrefixOf` text)
+          && ("where" `Text.isPrefixOf`) text =
+        (ln, text) : enumeratedPrepend ln
+      | otherwise = [(ln, text)]
 
 lineTag :: FileName -> LineNumber -> Text
 lineTag (FileName fn) (LineNumber ln) =
   "{-# LINE "
-  <> show ln
-  <> " \""
-  <> fn
-  <> "\" #-}\n"
-
+    <> show ln
+    <> " \""
+    <> fn
+    <> "\" #-}\n"
 
 renderImport :: FileName -> LineNumber -> RequireInfo -> Text
 renderImport filename linenumber RequireInfo {..} =
-  case (Text.isInfixOf riFullModuleName (unFileName filename)) of
-    True  -> ""
-    False -> typesImport <> lineTag filename linenumber <> qualifiedImport
- where
-  types = maybe (Text.takeWhileEnd (/= '.') riFullModuleName) (Text.intercalate ",") riImportedTypes
-  typesImport = "import " <> riFullModuleName <> " (" <> types <> ")\n"
-  qualifiedImport = "import qualified " <> riFullModuleName <> " as " <> riModuleAlias <> "\n"
-
+  if Text.isInfixOf riFullModuleName (unFileName filename)
+    then ""
+    else typesImport <> lineTag filename linenumber <> qualifiedImport
+  where
+    types = maybe (Text.takeWhileEnd (/= '.') riFullModuleName) (Text.intercalate ",") riImportedTypes
+    typesImport = "import " <> riFullModuleName <> " (" <> types <> ")\n"
+    qualifiedImport = "import qualified " <> riFullModuleName <> " as " <> riModuleAlias <> "\n"
 
 requireParser :: Parser RequireInfo
 requireParser = do
   void $ Megaparsec.string "require"
-  void $ Megaparsec.space1
+  void Megaparsec.space1
   module' <- Megaparsec.some (Megaparsec.alphaNumChar <|> Megaparsec.punctuationChar)
-  void $ Megaparsec.space
-
-  alias'  <- Megaparsec.try $ Megaparsec.option Nothing $ do
+  void Megaparsec.space
+  alias' <- Megaparsec.try $ Megaparsec.option Nothing $ do
     void $ Megaparsec.string "as"
-    void $ Megaparsec.space1
-    Just <$> Megaparsec.some (Megaparsec.alphaNumChar)
-
-  void $ Megaparsec.space
-
-  types'  <- Megaparsec.option Nothing $ do
+    void Megaparsec.space1
+    Just <$> Megaparsec.some Megaparsec.alphaNumChar
+  void Megaparsec.space
+  types' <- Megaparsec.option Nothing $ do
     void $ Megaparsec.char '('
     t' <- Megaparsec.many (Megaparsec.alphaNumChar <|> Megaparsec.char ',' <|> Megaparsec.char ' ')
     void $ Megaparsec.char ')'
     return $ Just t'
-
   void $ Megaparsec.option Nothing $ do
-    void $ Megaparsec.space
+    void Megaparsec.space
     void $ Megaparsec.some (Megaparsec.char '-')
     void $ Megaparsec.many (Megaparsec.alphaNumChar <|> Megaparsec.char ' ')
     return Nothing
-
-  return RequireInfo
-    { riFullModuleName = toText $ module'
-    , riModuleAlias    = maybe (Text.takeWhileEnd (/= '.') $ toText module') toText alias'
-    , riImportedTypes  = fmap Text.strip <$> Text.splitOn "," <$> toText <$> types'
-    }
+  return
+    RequireInfo
+      { riFullModuleName = toText module',
+        riModuleAlias = maybe (Text.takeWhileEnd (/= '.') $ toText module') toText alias',
+        riImportedTypes = (fmap Text.strip <$> Text.splitOn ",") . toText <$> types'
+      }
--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -1,81 +1,69 @@
-import           Universum
-
-import qualified Data.Text        as Text
-import qualified Test.Tasty
-import           Test.Tasty.Hspec
-
+import qualified Data.Text as Text
+import Relude
 import qualified Require
-
+import qualified Test.Tasty
+import Test.Tasty.Hspec
 
 main :: IO ()
 main = do
-    test <- testSpec "require" spec
-    Test.Tasty.defaultMain test
-
+  test <- testSpec "require" spec
+  Test.Tasty.defaultMain test
 
 spec :: Spec
 spec = parallel $ do
   it "transforms the 'require' keyword into a properly qualified import" $ do
-    let input    = "require Data.Text"
+    let input = "require Data.Text"
     let expected = "import qualified Data.Text as Text"
-    let actual   = Require.transform False (Require.FileName "Foo.hs") "" input
+    let actual = Require.transform False (Require.FileName "Foo.hs") "" input
     expected `Text.isInfixOf` actual
-
   it "imports the type based on the module" $ do
-    let input    = "require Data.Text"
+    let input = "require Data.Text"
     let expected = "import Data.Text (Text)"
-    let actual   = Require.transform False (Require.FileName "Foo.hs") "" input
+    let actual = Require.transform False (Require.FileName "Foo.hs") "" input
     expected `Text.isInfixOf` actual
-
   it "keeps the rest of the content intact" $ do
-    let input                   = "module Foo where\nrequire Data.Text\nfoo = 42"
-    let expectedStart           = "{-# LINE 1"
-    let expectedModule          = "module Foo where"
-    let expectedTypeImport      = "import Data.Text (Text)"
+    let input = "module Foo where\nrequire Data.Text\nfoo = 42"
+    let expectedStart = "{-# LINE 1"
+    let expectedModule = "module Foo where"
+    let expectedTypeImport = "import Data.Text (Text)"
     let expectedQualifiedImport = "import qualified Data.Text as Text"
-    let expectedContent         = "foo = 42\n"
-    let actual                  = toString $ Require.transform False (Require.FileName "Foo.hs") "" input
+    let expectedContent = "foo = 42\n"
+    let actual = toString $ Require.transform False (Require.FileName "Foo.hs") "" input
     actual `shouldStartWith` expectedStart
-    actual `shouldContain`   expectedModule
-    actual `shouldContain`   expectedTypeImport
-    actual `shouldContain`   expectedQualifiedImport
-    actual `shouldEndWith`   expectedContent
-
+    actual `shouldContain` expectedModule
+    actual `shouldContain` expectedTypeImport
+    actual `shouldContain` expectedQualifiedImport
+    actual `shouldEndWith` expectedContent
   it "aliases the modules properly" $ do
     let input = "require Data.Text as Foo"
     let expectedTypeImport = "import Data.Text (Text)"
     let expectedQualifiedImport = "import qualified Data.Text as Foo"
     let actual = toString $ Require.transform False (Require.FileName "Foo.hs") "" input
-    actual `shouldContain`   expectedTypeImport
-    actual `shouldContain`   expectedQualifiedImport
-
+    actual `shouldContain` expectedTypeImport
+    actual `shouldContain` expectedQualifiedImport
   it "imports the types properly" $ do
     let input = "require Data.Text (Foo)"
     let expectedTypeImport = "import Data.Text (Foo)"
     let expectedQualifiedImport = "import qualified Data.Text as Text"
     let actual = toString $ Require.transform False (Require.FileName "Foo.hs") "" input
-    actual `shouldContain`   expectedTypeImport
-    actual `shouldContain`   expectedQualifiedImport
-
+    actual `shouldContain` expectedTypeImport
+    actual `shouldContain` expectedQualifiedImport
   it "imports the types and aliases the modules properly" $ do
     let input = "require Data.Text as Quux (Foo)"
     let expectedTypeImport = "import Data.Text (Foo)"
     let expectedQualifiedImport = "import qualified Data.Text as Quux"
     let actual = toString $ Require.transform False (Require.FileName "Foo.hs") "" input
-    actual `shouldContain`   expectedTypeImport
-    actual `shouldContain`   expectedQualifiedImport
-
+    actual `shouldContain` expectedTypeImport
+    actual `shouldContain` expectedQualifiedImport
   it "skips comments" $ do
-    let input    = "require Data.Text -- test of comments"
+    let input = "require Data.Text -- test of comments"
     let expected = "import Data.Text (Text)"
-    let actual   = Require.transform False (Require.FileName "Foo.hs") "" input
+    let actual = Require.transform False (Require.FileName "Foo.hs") "" input
     expected `Text.isInfixOf` actual
-
   it "allows empty parentheses" $ do
-    let input       = "require Data.Text ()"
-    let expected1   = "import Data.Text ()"
-    let expected2   = "import qualified Data.Text as Text"
-    let actual      = lines $ Require.transform False (Require.FileName "Foo.hs") "" input
+    let input = "require Data.Text ()"
+    let expected1 = "import Data.Text ()"
+    let expected2 = "import qualified Data.Text as Text"
+    let actual = lines $ Require.transform False (Require.FileName "Foo.hs") "" input
     actual `shouldSatisfy` elem expected1
     actual `shouldSatisfy` elem expected2
-
